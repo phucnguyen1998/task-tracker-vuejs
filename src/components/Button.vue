4d3ef28e56d0483e3cc6c30dbd993817faabf9bb
ThaiSonNguyenTl <thaisonnguyentl@gmail.com>
2021-05-14 17:49:19 +0700

    cmm

---
+++ src/components/Button.vue
@@ -0,0 +1,22 @@
+<template>
+  <button v-on:click="onClick()" :style="{background: color}" class="btn">{{text}}</button>
+</template>
+
+<script>
+export default {
+    name: 'Button',
+    props: {
+        text: String,
+        color: String
+    },
+    methods:{
+        onClick(){
+            this.$emit('toggle-add-task')
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
